18b40cb73d7a53a44d7a373c285e56c41ead6944
YuraSadilenko <yura.sadilenko.it@gmail.com>
2018-12-15 11:27:29 +0200

    second task is ready

--- car.js
+++ car.js
@@ -1,23 +1,46 @@
-var Car = {
+var car = {
   mark: 'Mersedes',
   color: 'red',
   year: '2018',
   mileage: '1000',
-}
-
-function getStartEngine() {
-  var startEngine = true;
-  return startEngine;
-}
-console.log(getStartEngine());
-
-function getStartMove(startMove) {
-  if (getStartEngine() == true) {
-    startMove = true;
-  } else {
-    startMove = false;
+  isEngineStart: false,
+  isStartMove: false,
+  isStopMove: false,
+  amountOfFuel: 26,
+  startEngine: function() {
+    this.isEngineStart = true;
+  },
+  startMove: function() {
+    if (this.isEngineStart) {
+      this.isStartMove = true;
+      console.log('Машина марки ' + car.mark +',' + car.color +  ' цвета поехала!');
+    } else {
+      console.log('Включите вначале зажигание');
+    }
+    if (this.amountOfFuel < 10) {
+      console.log('Вам нужно заправить свой автомобиль!');
+    }
+    
+  },
+  stopMoving: function() {
+    if (this.isEngineStart) {
+      this.isStopMove = true;
+      console.log('Машинка остановилась')
+    } else {
+      console.log('Зажигание и так выключено')
+    }
+  },
+  refuelling: function() {
+    if (this.amountOfFuel < 10) {
+      this.amountOfFuel = this.amountOfFuel + 50;
+      console.log(this.amountOfFuel);
+    } else {
+      console.log('Вам не нужна заправка')
+    }
   }
-  return startMove;
 }
 
-console.log(getStartMove());
+console.log(car.startEngine());
+console.log(car.startMove());
+console.log(car.stopMoving());
+console.log(car.refuelling());
---
+++ pupils.js
@@ -0,0 +1,6 @@
+var pupils = {
+  anna: 29, 
+  misha: 35, 
+  stepan: 1, 
+  elena: 99
+}
