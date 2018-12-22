163b93ce553e11489811ee19297c3e3cc59770c8
YuraSadilenko <yura.sadilenko.it@gmail.com>
2018-12-22 17:19:29 +0200

    array ready

---
+++ array.js
@@ -0,0 +1,27 @@
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+function randomArray() {
+  return Math.random();
+}
+
+function sumOfArray() {
+  var sum = 0;
+  for ( var i = 0; i < array.length; i++) {
+    sum = sum + array[i];
+  }
+  return sum
+}
+
+function getMaxValue() {
+  var max = array[0]; 
+  for (var i = 0; i < array.length; i++) { 
+      if (max < array[i]) max = array[i]; 
+  }
+  return max;
+}
+
+array.sort(randomArray);
+console.log(array);
+console.log(sumOfArray());
+console.log(getMaxValue());
+
--- pupils.js
+++ pupils.js
@@ -2,5 +2,73 @@ var pupils = {
   anna: 29, 
   misha: 35, 
   stepan: 1, 
-  elena: 99
+  elena: 99,
 }
+
+var bestMark = 0;
+var pupilName = "";
+
+function bestMark() {
+  for (name in pupils) {
+    if(
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+    )
+  }  
+}
+
+
+console.log(pupils.bestMark())
