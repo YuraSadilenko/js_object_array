1ef0b23b83b78f5491a5feabcec7950b225f8c23
YuraSadilenko <yura.sadilenko.it@gmail.com>
2018-12-25 15:45:01 +0200

    fixes

--- js/subArraySum.js
+++ js/subArraySum.js
@@ -1,15 +1,15 @@
-var array = [1, -2, 3, 4, -9, 6];
+var array = [1, -2, 3, 4, -9, 9, 6];
 
-console.log(getSumOfSubArray(array));
+getSumOfSubArray(array);
 
 function getSumOfSubArray(arr) {
   var maxSum = 0;
-  for (var i = 1; i <= arr.length; i++) {
+  for (var i = 0; i < arr.length; i++) {
     var subArraySum = 0;
-    for (var j = i; j <= arr.length - i; j++) {
+    for (var j = i; j < arr.length; j++) {
       subArraySum += arr[j];
       maxSum = Math.max(maxSum, subArraySum);
     }
   }
-  return maxSum;
+  console.log(maxSum);
 }
