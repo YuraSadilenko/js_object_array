67d998c6ade1dde0a9869593a385101fed5c3970
YuraSadilenko <yura.sadilenko.it@gmail.com>
2018-12-26 21:39:35 +0200

    commit

--- js/subArraySum.js
+++ js/subArraySum.js
@@ -3,8 +3,7 @@ var array = [1, -2, 3, 4, -9, 11, 6];
 console.log(getMaxSubSum(array));
 
 function getMaxSubSum(arr) {
-  var maxSum = 0; // если совсем не брать элементов, то сумма 0
-
+  var maxSum = 0; 
   for (var i = 0; i < arr.length; i++) {
     var sumFixedStart = 0;
     for (var j = i; j < arr.length; j++) {
--- js/subarray.js
+++ js/subarray.js
@@ -1,10 +1,9 @@
-var array = [1, 2, 3, 4];
+var array = [1, -2, 3, 4, -9, 11, 6];
 
 console.log(getSubArrays(array));
 
 function getSubArrays(arr) {
   var subArray = [];
-
   for (var i = 1; i <= arr.length; i++) {
     for (var j = 0; j <= arr.length - i; j++) {
       subArray.push(arr.slice(j, j + i));
